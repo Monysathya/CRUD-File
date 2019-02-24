1c6cfb90bc2ef2dda7c396461cc4d57d5d8f6d09
Monysathya <monysathya.suos@sompom.com>
2019-02-24 10:45:54 +0700

    add project to repository

---
+++ CRUD-Upload-file/Models/FileDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CRUD_Upload_file.Models
+{
+    public class FileDetail
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public int SupportId { get; set; }
+        public virtual Support Support { get; set; }
+    }
+}
---
+++ CRUD-Upload-file/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD_Upload_file.Startup))]
+namespace CRUD_Upload_file
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
